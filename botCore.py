f22e929eb16e2c0f1d0bad8efd33199fa9c87de5
xana8u <xana8u@gmail.com>
2018-04-27 02:39:26 +0300

    typo fix

--- botCore.py
+++ botCore.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 
 
 bot = commands.Bot(command_prefix="!")
-allowed_roles = ["knight", "wizard", "priest", "berzerker",
+allowed_roles = ["knight", "wizard", "priest", "berserker",
                  "heavy gunner", "assassin", "thief", "archer"]
 
 @bot.event
